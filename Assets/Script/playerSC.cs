3be00b4a9abbb81aa14026a8ad3994bf0f484fc1
Dzolver <kaitoano0@gmail.com>
2018-06-21 10:52:58 +0530

    Point and Click is implemented with Chat UI

---
+++ Assets/Script/playerSC.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSC : MonoBehaviour {
+	public float _speed;
+	private bool _move = false;
+	public GameObject _point;
+	private Vector3 _target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			_target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			_target.z = transform.position.z;
+			if (_move == false)
+				_move = true;
+			Instantiate (_point, _target, Quaternion.identity);
+		}
+		if (_move == true) {
+			transform.position = Vector3.MoveTowards (transform.position,_target, _speed * Time.deltaTime);
+		}
+	}
+}
